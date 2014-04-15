90158e1fb5a31e53830d132126b5c3de6cbd13c2
Joe Ferris <jferris@thoughtbot.com>
2014-04-15 17:50:10 -0400

    Allow defining dependencies without a method chain
    * Forgetting a dot would undefine all previous dependencies
    * Encapsulates mutation within a builder class
    * Application still references immutable state

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,97 +1,122 @@
-factory :authenticator do |config|
-  Authenticator.new(config[:auth_hash])
+factory :authenticator do |container|
+  Authenticator.new(container[:auth_hash])
 end
-.decorate :authenticator do |component, config|
+
+decorate :authenticator do |component, container|
   Gitolite::PublicKeyAuthenticator.new(
     component,
-    config[:auth_hash],
-    config[:public_key_syncronizer]
+    container[:auth_hash],
+    container[:public_key_syncronizer]
   )
 end
-.factory :public_key_syncronizer do |config|
+
+factory :public_key_syncronizer do |container|
   Gitolite::PublicKeySyncronizer.new(
-    server: config[:git_server],
-    local_keys: config[:local_keys],
-    remote_keys: config[:remote_keys],
-    user: config[:user]
+    server: container[:git_server],
+    local_keys: container[:local_keys],
+    remote_keys: container[:remote_keys],
+    user: container[:user]
   )
 end
-.service :git_server do |config|
+
+service :git_server do |container|
   Gitolite::Server.new(
-    config_committer: config[:config_committer],
-    repository_finder: config[:repository_finder]
+    config_committer: container[:config_committer],
+    repository_finder: container[:repository_finder]
   )
 end
-.service :config_committer do |config|
+
+service :config_committer do |container|
   Gitolite::ConfigCommitter.new(
-    repository_factory: config[:repository],
-    config_writer: config[:config_writer]
+    repository_factory: container[:repository],
+    config_writer: container[:config_writer]
   )
 end
-.service :config_writer do |config|
-  Gitolite::ConfigWriter.new(ENV['PUBLIC_KEY'], config[:sources])
+
+service :config_writer do |container|
+  Gitolite::ConfigWriter.new(ENV['PUBLIC_KEY'], container[:sources])
 end
-.service :sources do |config|
-  Gitolite::SourceCollection.new(config[:exercises], config[:repository_finder])
+
+service :sources do |container|
+  Gitolite::SourceCollection.new(
+    container[:exercises],
+    container[:repository_finder]
+ )
 end
-.service :exercises do |config|
+
+service :exercises do |container|
   Exercise.alphabetical
 end
-.decorate :exercises do |exercises, config|
+
+decorate :exercises do |exercises, container|
   Gitolite::ReloadingCollection.new(exercises)
 end
-.service :shell do |config|
+
+service :shell do |container|
   ENV['SHELL_CLASS'].constantize.new
 end
-.decorate :shell do |shell, config|
+
+decorate :shell do |shell, container|
   Gitolite::IdentifiedShell.new(shell, ENV['IDENTITY'])
 end
-.service :git_observer do |config|
-  Git::Observer.new(config[:git_server])
+
+service :git_observer do |container|
+  Git::Observer.new(container[:git_server])
 end
-.factory :participation do |config|
+
+factory :participation do |container|
   Participation.new(
-    config[:exercise],
-    config[:user],
-    config[:git_observer]
+    container[:exercise],
+    container[:user],
+    container[:git_observer]
   )
 end
-.factory :repository do |config|
-  Git::Repository.new(host: ENV['GIT_SERVER_HOST'], path: config[:path])
+
+factory :repository do |container|
+  Git::Repository.new(host: ENV['GIT_SERVER_HOST'], path: container[:path])
 end
-.service :repository_finder do |config|
-  Gitolite::RepositoryFinder.new(config[:repository])
+
+service :repository_finder do |container|
+  Gitolite::RepositoryFinder.new(container[:repository])
 end
-.decorate :repository do |component, config|
-  Gitolite::ClonableRepository.new(component, config[:shell])
+
+decorate :repository do |component, container|
+  Gitolite::ClonableRepository.new(component, container[:shell])
 end
-.decorate :repository do |component, config|
-  Gitolite::ForkableRepository.new(component, config[:shell])
+
+decorate :repository do |component, container|
+  Gitolite::ForkableRepository.new(component, container[:shell])
 end
-.decorate :repository do |component, config|
-  Gitolite::CommittableRepository.new(component, config[:shell])
+
+decorate :repository do |component, container|
+  Gitolite::CommittableRepository.new(component, container[:shell])
 end
-.decorate :repository do |component, config|
-  Gitolite::RepositoryWithHistory.new(component, config[:shell])
+
+decorate :repository do |component, container|
+  Gitolite::RepositoryWithHistory.new(component, container[:shell])
 end
-.decorate :repository do |component, config|
-  Gitolite::DiffableRepository.new(component, config[:shell])
+
+decorate :repository do |component, container|
+  Gitolite::DiffableRepository.new(component, container[:shell])
 end
-.factory :review do |config|
+
+factory :review do |container|
   viewed_solution = Git::Solution.new(
-    config[:viewed_solution],
-    config[:diff_parser]
+    container[:viewed_solution],
+    container[:diff_parser]
   )
 
   Review.new(
-    exercise: config[:exercise],
-    submitted_solution: config[:submitted_solution],
+    exercise: container[:exercise],
+    submitted_solution: container[:submitted_solution],
     viewed_solution: viewed_solution
   )
 end
-.factory :diff_parser do |config|
-  Git::DiffParser.new(config[:diff], config[:diff_file])
+
+factory :diff_parser do |container|
+  Git::DiffParser.new(container[:diff], container[:diff_file])
 end
-.factory :diff_file do |config|
+
+factory :diff_file do |container|
   Git::DiffFile.new
 end
