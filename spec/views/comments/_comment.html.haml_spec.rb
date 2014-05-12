010500413ab43a64fd9b8305425c7783c06c6633
Joe Ferris <jferris@thoughtbot.com>
2014-05-12 13:00:48 -0400

    Add word "ago" to comment times
    Changes "4 minutes" to "4 minutes ago"
    
    https://trello.com/c/O2mWRJZQ/60-bug-comment-timestamps-say-one-minute-instead-of-one-minute-ago

--- spec/views/comments/_comment.html.haml_spec.rb
+++ spec/views/comments/_comment.html.haml_spec.rb
@@ -10,4 +10,18 @@ describe 'comments/_comment.html' do
 
     expect(rendered).to include('rendered avatar')
   end
+
+  it 'describes when the comment was created' do
+    Timecop.freeze Time.now do
+      comment = build_stubbed(:comment, created_at: 5.minutes.ago)
+
+      render comment
+
+      expect(rendered_words).to include('5 minutes ago')
+    end
+  end
+
+  def rendered_words
+    Capybara.string(rendered).text.split(/\s+/).join(' ')
+  end
 end
