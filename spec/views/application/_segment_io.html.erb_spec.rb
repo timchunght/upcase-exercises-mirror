0a59cd61f2a64db9d8deb17dc8e0409fba59db95
Ben Orenstein <ben.orenstein@gmail.com>
2014-05-13 11:29:28 -0400

    Add Segment.io snippet and identify users
    * The snippet will only be included if ENV['SEGMENT_IO_KEY'] is set.
    * Signed-in users will be identified.
    * Removes a stray Wistia embed.

---
+++ spec/views/application/_segment_io.html.erb_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe 'application/_segment_io.html.erb' do
+  context 'when analytics are active' do
+    it 'loads the analytics' do
+      with_analytics_enabled('fake-key') do
+        render 'segment_io', user: nil
+      end
+
+      expect(rendered).to include('fake-key')
+    end
+
+    context 'when a user is signed in' do
+      it 'identifies the signed-in user' do
+        user = build_stubbed(:user)
+
+        with_analytics_enabled do
+          render 'segment_io', user: user
+        end
+
+        expect(rendered).to include(user.id.to_s)
+        expect(rendered).to include(user.email)
+        expect(rendered).to include(user.first_name)
+      end
+    end
+  end
+
+  context 'when analytics are not enabled' do
+    it 'does not load analytics' do
+      render 'segment_io'
+
+      expect(rendered).to be_blank
+    end
+  end
+
+  def with_analytics_enabled(segment_io_key = 'fake-key', &block)
+    options = { SEGMENT_IO_KEY: segment_io_key }
+    ClimateControl.modify(options, &block)
+  end
+end
