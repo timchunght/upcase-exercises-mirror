12dd9087e0396831ec6ad649e8b0a9a6e06da4b4
Joe Ferris <jferris@thoughtbot.com>
2014-05-14 15:36:42 -0400

    Trace time spend in OmniAuth layer
    * New Relic doesn't track anything in Rack middleware by default
    * We're performing HTTP requests in OmniAuth's middleware

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,11 @@ LEARN_CLIENT_SECRET = ENV['LEARN_CLIENT_SECRET']
 LEARN_URL = ENV['LEARN_URL']
 
 require 'omniauth-learn'
+require 'new_relic/agent/instrumentation/rack'
+
+OmniAuth::Builder.class_eval do
+  include ::NewRelic::Agent::Instrumentation::Rack
+end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
