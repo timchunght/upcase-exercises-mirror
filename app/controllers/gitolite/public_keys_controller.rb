5823b98d80910c51d8ad3963a71aaf98049081cf
Joe Ferris <jferris@thoughtbot.com>
2014-05-21 17:09:50 -0400

    Write public key to Gitolite after adding
    * Creating a PublicKey record only updates the local cache
    * We need to rewrite the Gitolite config to actually provide access
    
    https://trello.com/c/XUAcde0I/83-as-a-subscriber-i-want-to-upload-a-public-key-if-none-were-downloaded-from-github-so-that-i-will-have-access-to-exercises-i-clon

--- app/controllers/gitolite/public_keys_controller.rb
+++ app/controllers/gitolite/public_keys_controller.rb
@@ -5,6 +5,7 @@ class Gitolite::PublicKeysController < ApplicationController
 
   def create
     @public_key = create_public_key
+    add_public_key_to_gitolite
     redirect_back_or(root_url)
   end
 
@@ -12,6 +13,10 @@ class Gitolite::PublicKeysController < ApplicationController
 
   def create_public_key
     dependencies[:current_public_keys].create!(data: public_key_data)
+  end
+
+  def add_public_key_to_gitolite
+    dependencies[:git_server].add_key(current_user.username)
   end
 
   def public_key_data
---
+++ spec/controllers/gitolite/public_keys_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Gitolite::PublicKeysController do
+  describe '#create' do
+    it 'creates the key and updates Gitolite' do
+      git_server = stub_service(:git_server)
+      git_server.stub(:add_key)
+      user = build_stubbed(:user)
+      public_keys = stub_service(:current_public_keys)
+      public_keys.stub(:create!)
+
+      sign_in_as user
+      post :create, gitolite_public_key: { data: 'ssh-rsa 123' }
+
+      expect(public_keys).to have_received(:create!).with(data: 'ssh-rsa 123')
+      expect(git_server).to have_received(:add_key).with(user.username)
+    end
+  end
+end
