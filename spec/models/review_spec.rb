timchunght/upcase-exercises-mirror

commit de51d672a9a243a000be181d61cb5341a3905856

Users are now guided through a review workflow
* Status messages are now shown in the header, along with calls-to-action like
"Continue" or "Next exercise"
* A user moves through Submitted, ReviewingSolution, AwaitingReview, and
AllStepsCompleted states.
* This commit introduces a `Status` namespace.
* `Status::Finder` looks for the appropriate Status to show, choosing
the first `Status::X` class that returns `true` for a `#applicable?`
method. This class' `to_partial_path` method is called to render the
appropriate partial.
* Commits including styles and markup for an unimplemented progress
bar were pushed to this branch, so one call to `render 'progress_bar'`
is commented out.

Possibly surprising:

This commit ignores whether user has commented on assigned solution.
Instead, we look at whether they've commented on _any_ solution.

This is necessary since a user's assigned solution changes as soon as
they review a solution. I'd call this a bug, or at least poor UX (the
"review this solution" prompt switches solutions and never disappears),
but fixing it is out of the scope of this feature.

I've created
https://trello.com/c/bHjpOxyb/109 to track this additional work.

http://trello.com/c/y9hKVEFb/78

app/models/review.rb
@@ -3,9 +3,11 @@ class Review
   pattr_initialize([
     :comment_locator,
     :exercise,
+    :reviewer,
     :solutions,
+    :status_factory,
     :submitted_solution,
-    :viewed_solution
+    :viewed_solution,
   ])
   attr_reader :exercise, :viewed_solution
 
@@ -15,6 +17,10 @@ class Review
 
   def assigned_solver
     assigned_solution.user
+  end
+
+  def assigned_solver_username
+    assigned_solution.username
   end
 
   def has_solutions_by_other_users?
@@ -45,7 +51,32 @@ class Review
     comment_locator.top_level_comments
   end
 
+  def status
+    status_factory.new(review: self)
+  end
+
+  def viewing_other_solution?
+    viewed_solution != submitted_solution
+  end
+
+  def user_is_awaiting_review?
+    !submitted_solution.has_comments?
+  end
+
+  def user_has_reviewed_other_solution?
+    exercise.has_comments_from?(reviewer)
+  end
+
+  def user_has_given_and_received_review?
+    user_has_reviewed_other_solution? &&
+      user_has_received_review?
+  end
+
   private
+
+  def user_has_received_review?
+    !user_is_awaiting_review?
+  end
 
   def decorate_solutions(solutions)
     solutions.each_with_index.map do |solution, index|

spec/models/review_spec.rb
@@ -12,6 +12,19 @@ describe Review do
     end
   end
 
+  describe '#status' do
+    it 'build status from its factory' do
+      status_factory = double('status_factory')
+      status = double('status')
+      status_factory.stub(:new).and_return(status)
+      review = build_review(status_factory: status_factory)
+
+      result = review.status
+
+      expect(result).to eq(status)
+    end
+  end
+
   describe '#has_solutions_by_other_users?' do
     it "returns true with solutions besides the current user's" do
       other_solution = stub_solution('other_solution')
@@ -106,6 +119,23 @@ describe Review do
     end
   end
 
+  describe '#assigned_solver_username' do
+    it 'returns the username of the first solver besides the reviewer' do
+      submitted_solution = stub_solution('submitted_solution')
+      other_solutions = [
+        stub_solution('first_other', username: 'first username'),
+        stub_solution('second_other', username: 'second username')
+      ]
+      review = build_review(
+        other_solutions: other_solutions,
+        submitted_solution: submitted_solution,
+        viewed_solution: stub_solution('viewed_solution'),
+      )
+
+      expect(review.assigned_solver_username).to eq 'first username'
+    end
+  end
+
   describe '#assigned_solver' do
     it 'returns the first solver besides the reviewer' do
       submitted_solution = stub_solution('submitted_solution')
@@ -119,7 +149,7 @@ describe Review do
         viewed_solution: stub_solution('viewed_solution'),
       )
 
-      expect(review.assigned_solver).to eq 'first user'
+      expect(review.assigned_solver).to eq('first user')
     end
   end
 
@@ -164,12 +194,158 @@ describe Review do
     end
   end
 
+  describe '#viewing_other_solution?' do
+    context "when viewing a solution other than the user's" do
+      it 'returns true' do
+        submitted_solution = stub_solution('submitted_solution')
+        viewed_solution = stub_solution('viewed_solution')
+        review = build_review(
+                   viewed_solution: viewed_solution,
+                   submitted_solution: submitted_solution
+                 )
+
+        result = review.viewing_other_solution?
+
+        expect(result).to be_true
+      end
+    end
+
+    context "when viewing the user's solution" do
+      it 'returns false' do
+        solution = stub_solution('solution')
+        review = build_review(
+                   viewed_solution: solution,
+                   submitted_solution: solution
+                 )
+
+        result = review.viewing_other_solution?
+
+        expect(result).to be_false
+      end
+    end
+  end
+
+  describe '#user_is_awaiting_review?' do
+    context "when the user's solution has no comments" do
+      it "returns true" do
+        solution_with_no_comments =
+          stub_solution('no_comments_solution', has_comments?: false)
+
+        review = build_review(submitted_solution: solution_with_no_comments)
+
+        expect(review.user_is_awaiting_review?).to be_true
+      end
+    end
+
+    context "when the user's solution has comments" do
+      it "returns false" do
+        solution_with_comments =
+          stub_solution('solution_with_comments', has_comments?: true)
+
+        review = build_review(submitted_solution: solution_with_comments)
+
+        expect(review.user_is_awaiting_review?).to be_false
+      end
+    end
+  end
+
+  describe '#user_has_reviewed_other_solution?' do
+    context 'when the user has commented on another solution' do
+      it 'returns true' do
+        user = double('user')
+        exercise = double('exercise')
+        exercise.stub(:has_comments_from?).with(user).and_return(true)
+
+        review = build_review(
+                   reviewer: user,
+                   exercise: exercise,
+                 )
+
+        expect(review.user_has_reviewed_other_solution?).to be_true
+      end
+    end
+
+    context 'when the user has not commend on another solution' do
+      it 'returns false' do
+        user = double('user')
+        exercise = double('exercise')
+        exercise.stub(:has_comments_from?).with(user).and_return(false)
+
+        review = build_review(
+                   reviewer: user,
+                   exercise: exercise,
+                 )
+
+        expect(review.user_has_reviewed_other_solution?).to be_false
+      end
+    end
+  end
+
+  describe '#user_has_given_and_received_review?' do
+    context 'when the user has given and received a review' do
+      it 'returns true' do
+        user = double('user')
+        submitted_solution = stub_solution('submitted_solution', user: user)
+        submitted_solution.stub(:has_comments?).and_return(true)
+        exercise = double('exercise')
+        exercise.stub(:has_comments_from?).with(user).and_return(true)
+
+        review = build_review(
+                   submitted_solution: submitted_solution,
+                   reviewer: user,
+                   exercise: exercise,
+                 )
+
+        expect(review.user_has_given_and_received_review?).to be_true
+      end
+    end
+
+    context 'when the user has not given a review' do
+      it 'returns false' do
+        user = double('user')
+        submitted_solution = stub_solution('submitted_solution', user: user)
+        submitted_solution.stub(:has_comments?).and_return(true)
+        exercise = double('exercise')
+        exercise.stub(:has_comments_from?).with(user).and_return(false)
+
+        review = build_review(
+                   submitted_solution: submitted_solution,
+                   reviewer: user,
+                   exercise: exercise,
+                 )
+
+        expect(review.user_has_given_and_received_review?).to be_false
+      end
+    end
+
+    context 'when the user has not receieved a review' do
+      it 'returns false' do
+        user = double('user')
+        submitted_solution = stub_solution('submitted_solution', user: user)
+        submitted_solution.stub(:has_comments?).and_return(false)
+        exercise = double('exercise')
+        exercise.stub(:has_comments_from?).with(user).and_return(false)
+
+        review =
+          build_review(
+            submitted_solution: submitted_solution,
+            reviewer: user,
+            exercise: exercise,
+          )
+
+        expect(review.user_has_given_and_received_review?).to be_false
+      end
+    end
+  end
+
   def build_review(
     comment_locator: double('comment_locator'),
     submitted_solution: stub_solution('submitted_solution'),
     other_solutions: [stub_solution('other_solution')],
     viewed_solution: submitted_solution || other_solutions.first,
-    exercise: double('exercise')
+    exercise: double('exercise'),
+    status_factory: double('status_factory'),
+    reviewer: double('user')
   )
     solutions = [submitted_solution, *other_solutions].compact
     Review.new(
@@ -177,7 +353,9 @@ describe Review do
       exercise: exercise,
       solutions: solutions,
       submitted_solution: submitted_solution,
-      viewed_solution: viewed_solution
+      viewed_solution: viewed_solution,
+      status_factory: status_factory,
+      reviewer: reviewer,
     )
   end
 

app/assets/images/clock.svg
@@ -1,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg width="50px" height="50px" viewBox="0 0 50 50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+    <!-- Generator: Sketch 3.0.2 (7799) - http://www.bohemiancoding.com/sketch -->
+    <title>icon</title>
+    <description>Created with Sketch.</description>
+    <defs></defs>
+    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+        <g id="icon" sketch:type="MSLayerGroup" fill="#CDA970">
+            <path d="M25,0 C11.1944444,0 0,11.1944444 0,25 C0,38.8055556 11.1944444,50 25,50 C38.8055556,50 50,38.8055556 50,25 C50,11.1944444 38.8055556,0 25,0 L25,0 Z M25,47.5 C12.574375,47.5 2.5,37.425625 2.5,25 C2.5,12.574375 12.574375,2.5 25,2.5 C37.425625,2.5 47.5,12.574375 47.5,25 C47.5,37.425625 37.425625,47.5 25,47.5 L25,47.5 Z" id="Shape" sketch:type="MSShapeGroup"></path>
+            <path d="M26.3888889,5.55555556 L26.3888889,11.1111111 C26.3888889,11.8777778 25.7666667,12.5 25,12.5 C24.2333333,12.5 23.6111111,11.8777778 23.6111111,11.1111111 L23.6111111,5.55555556 C23.6111111,4.78888889 24.2333333,4.16666667 25,4.16666667 C25.7666667,4.16666667 26.3888889,4.78888889 26.3888889,5.55555556 L26.3888889,5.55555556 Z" id="Shape" sketch:type="MSShapeGroup"></path>
+            <path d="M26.3888889,41.3888889 L26.3888889,44.1666667 C26.3888889,44.9333333 25.7666667,45.5555556 25,45.5555556 C24.2333333,45.5555556 23.6111111,44.9333333 23.6111111,44.1666667 L23.6111111,41.3888889 C23.6111111,40.6222222 24.2333333,40 25,40 C25.7666667,40 26.3888889,40.6222222 26.3888889,41.3888889 L26.3888889,41.3888889 Z" id="Shape" sketch:type="MSShapeGroup"></path>
+            <path d="M44.1666667,26.3888889 L41.3888889,26.3888889 C40.6222222,26.3888889 40,25.7666667 40,25 C40,24.2333333 40.6222222,23.6111111 41.3888889,23.6111111 L44.1666667,23.6111111 C44.9333333,23.6111111 45.5555556,24.2333333 45.5555556,25 C45.5555556,25.7666667 44.9333333,26.3888889 44.1666667,26.3888889 L44.1666667,26.3888889 Z" id="Shape" sketch:type="MSShapeGroup"></path>
+            <path d="M8.05555556,26.3888889 L5.27777778,26.3888889 C4.51111111,26.3888889 3.88888889,25.7666667 3.88888889,25 C3.88888889,24.2333333 4.51111111,23.6111111 5.27777778,23.6111111 L8.05555556,23.6111111 C8.82222222,23.6111111 9.44444444,24.2333333 9.44444444,25 C9.44444444,25.7666667 8.82222222,26.3888889 8.05555556,26.3888889 L8.05555556,26.3888889 Z" id="Shape" sketch:type="MSShapeGroup"></path>
+            <circle id="Oval" sketch:type="MSShapeGroup" cx="39.1666667" cy="16.9444444" r="1.38888889"></circle>
+            <circle id="Oval" sketch:type="MSShapeGroup" cx="10.2777778" cy="33.6111111" r="1.38888889"></circle>
+            <circle id="Oval" sketch:type="MSShapeGroup" cx="33.0555556" cy="10.8333333" r="1.38888889"></circle>
+            <circle id="Oval" sketch:type="MSShapeGroup" cx="16.3888889" cy="39.1666667" r="1.38888889"></circle>
+            <circle id="Oval" sketch:type="MSShapeGroup" cx="16.3888889" cy="10.8333333" r="1.38888889"></circle>
+            <circle id="Oval" sketch:type="MSShapeGroup" cx="33.0555556" cy="39.1666667" r="1.38888889"></circle>
+            <circle id="Oval" sketch:type="MSShapeGroup" cx="10.2777778" cy="16.9444444" r="1.38888889"></circle>
+            <circle id="Oval" sketch:type="MSShapeGroup" cx="39.1666667" cy="33.6111111" r="1.38888889"></circle>
+            <path d="M30.7277778,24.95 L25.3888889,26.3333333 C24.8522222,26.4905556 24.1105556,26.2833333 23.8333333,25.7555556 L17.7944444,16.4277778 C17.3777778,15.7833333 17.5666667,14.9222222 18.2055556,14.5055556 C18.85,14.0888889 19.7111111,14.2722222 20.1277778,14.9166667 L25.6222222,23.4055556 L30.0277778,22.2611111 C30.7722222,22.0722222 31.5333333,22.5166667 31.7222222,23.2555556 C31.9166667,24 31.4666667,24.7611111 30.7277778,24.95 C30.7277778,24.95 31.4666667,24.7611111 30.7277778,24.95 L30.7277778,24.95 Z" id="Shape" sketch:type="MSShapeGroup"></path>
+        </g>
+    </g>
+</svg>

app/assets/images/comment-icon.svg
@@ -1,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg width="42px" height="37px" viewBox="0 0 42 37" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+    <!-- Generator: Sketch 3.0.2 (7799) - http://www.bohemiancoding.com/sketch -->
+    <title>comment 2</title>
+    <description>Created with Sketch.</description>
+    <defs></defs>
+    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+        <g id="comment-2" sketch:type="MSLayerGroup" transform="translate(1.000000, 1.000000)">
+            <path d="M2,0 C0.816031596,0 0,0.845282982 0,2 L0,26 C0,27.3945487 0.804836967,28.2438718 2,28 L6,28 L10,34 C11.2318811,35.200251 12.3103922,35.1922932 13,34 L18,28 L38,28 C39.1839589,28.2438718 40,27.3985889 40,26 L40,2 C40,0.84932324 39.2009849,0 38,0 L2,0 L2,0 Z" id="review_comment" stroke="#C49B61" stroke-width="2" sketch:type="MSShapeGroup"></path>
+            <path d="M7.79310345,7 C7.17441086,7 7,7.19907952 7,7.66666667 L7,8.33333333 C7,8.80127492 7.17190895,9 7.79310345,9 L29.2068966,9 C29.8255891,9 30,8.80092048 30,8.33333333 L30,7.66666667 C30,7.19872504 29.8280909,7 29.2068966,7 L7.79310345,7 L7.79310345,7 Z" id="review_comment-path" fill="#CDA970" sketch:type="MSShapeGroup"></path>
+            <path d="M7.79310345,13 C7.17441086,13 7,13.1990795 7,13.6666667 L7,14.3333333 C7,14.801275 7.17190895,15 7.79310345,15 L29.2068966,15 C29.8255891,15 30,14.8009205 30,14.3333333 L30,13.6666667 C30,13.198725 29.8280909,13 29.2068966,13 L7.79310345,13 L7.79310345,13 Z" id="review_comment-path" fill="#CDA970" sketch:type="MSShapeGroup"></path>
+            <path d="M7.80952381,19 C7.17693473,19 7,19.1990796 7,19.6666667 L7,20.3333333 C7,20.801275 7.17941547,21 7.80952381,21 L23.1904762,21 C23.8230651,21 24,20.8009204 24,20.3333333 L24,19.6666667 C24,19.198725 23.8205845,19 23.1904762,19 L7.80952381,19 L7.80952381,19 Z" id="review_comment-path" fill="#CDA970" sketch:type="MSShapeGroup"></path>
+        </g>
+    </g>
+</svg>

app/assets/images/finished-check.svg
@@ -1,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg width="11px" height="10px" viewBox="0 0 11 10" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+    <!-- Generator: Sketch 3.0.2 (7799) - http://www.bohemiancoding.com/sketch -->
+    <title>Shape</title>
+    <description>Created with Sketch.</description>
+    <defs></defs>
+    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+        <path d="M9.17957085,0.173503135 L2.65617085,7.51210314 L0.982570845,4.23925314 C0.837770845,3.95597814 0.515970845,3.85832814 0.263970845,4.02122814 C0.0117708454,4.18435314 -0.0746291546,4.54615314 0.0701708454,4.82965314 L2.08637085,8.77210314 C2.10897085,8.81642814 2.13657085,8.85512814 2.16697085,8.89000314 C2.16857085,8.89180314 2.16877085,8.89382814 2.17037085,8.89540314 C2.17057085,8.89585314 2.17117085,8.89607814 2.17137085,8.89630314 C2.21637085,8.94670314 2.26957085,8.98292814 2.32557085,9.01150314 C2.33957085,9.01870314 2.35297085,9.02522814 2.36737085,9.03062814 C2.42337085,9.05312814 2.48157085,9.06865314 2.54137085,9.06865314 C2.54157085,9.06865314 2.54217085,9.06910314 2.54237085,9.06910314 L2.54257085,9.06910314 L2.54277085,9.06910314 C2.60137085,9.06910314 2.65937085,9.05357814 2.71657085,9.03085314 C2.73177085,9.02477814 2.74577085,9.01802814 2.76057085,9.01060314 C2.77537085,9.00317814 2.79057085,8.99935314 2.80477085,8.99012814 C2.82557085,8.97640314 2.83817085,8.95480314 2.85677085,8.93860314 C2.87557085,8.92285314 2.89757085,8.91475314 2.91457085,8.89562814 L9.92377085,1.01050314 C10.1293708,0.779203135 10.1293708,0.404578135 9.92377085,0.173278135 C9.71837085,-0.0577968647 9.38517085,-0.0577968647 9.17957085,0.173503135 L9.17957085,0.173503135 Z" id="Shape" fill="#5A9817" sketch:type="MSShapeGroup"></path>
+    </g>
+</svg>

app/assets/stylesheets/_layout.scss
@@ -108,12 +108,12 @@ nav.exercise {
 .main-body {
   @include clearfix;
   margin-left: $sidebar-width;
-  max-width: 1400px;
-  min-width: 800px;
-  padding: 0 2rem 2rem 3rem;
+  max-width: $max-width-main;
+  min-width: $min-width-main;
+  padding: 0 $main-padding-sm 2rem $main-padding;
 
   @include media(max-width $preserve-80-char) {
     margin-left: $sidebar-width-sm;
-    padding-left: 2rem;
+    padding-left: $main-padding-sm;
   }
 }

app/assets/stylesheets/_variables.scss
@@ -24,6 +24,7 @@ $files-width: 65%;
 $timeline-width: 100% - $files-width;
 
 $main-padding: 3rem;
+$main-padding-sm: 2rem;
 
 $preserve-80-char: 1385px;
 $break-80-char: max-width (1385px - (16px*2));

app/assets/stylesheets/bitters/_variables.scss
@@ -45,3 +45,6 @@ $base-border-color-2: #efefef;
 $error-color: $light-red;
 $notice-color: $light-yellow;
 $success-color: $light-green;
+
+$max-width-main: 1400px;
+$min-width-main: 800px;

commit f6a790d2fc44ee9a194d963038618839063a06ca

Admins can comment on solutions they haven't submitted
* Fixes a bug introduced in de51d67 where admins who commented on a
solution that they hadn't submitted got an exception.

https://trello.com/c/hZPQ5U0o/117

app/models/review.rb
@@ -60,7 +60,9 @@ class Review
   end
 
   def user_is_awaiting_review?
-    !submitted_solution.has_comments?
+    if submitted?
+      submitted_solution_has_no_comments?
+    end
   end
 
   def user_has_reviewed_other_solution?
@@ -73,6 +75,10 @@ class Review
   end
 
   private
+
+  def submitted_solution_has_no_comments?
+    !submitted_solution.has_comments?
+  end
 
   def user_has_received_review?
     !user_is_awaiting_review?

spec/features/admin_views_solutions_spec.rb
@@ -1,33 +1,39 @@
 require 'spec_helper'
 
-feature 'admin views solutions' do
-  scenario 'before submitting solution' do
+feature 'admin views a solution' do
+  scenario 'and comments on it', js: true do
     exercise = create(:exercise)
-    other_user = create(:user, username: 'otheruser')
-    clone = create(:clone, exercise: exercise, user: other_user)
-    solution = create(:solution, clone: clone)
-    create(:revision, solution: solution)
+    other_user = create(:user, username: 'other_user')
+    admin = create(:admin)
+    workflow = start_exercise_workflow(
+                 exercise: exercise,
+                 user: admin,
+               )
+    workflow.create_solution_by_other_user(user: other_user)
 
-    visit_exercise exercise
-    click_on 'View Solutions'
+    visit_solutions_for(exercise: exercise, as: admin)
+    workflow.comment_on_solution('Looks good')
+    reload_page
 
+    expect(page).to have_content('Looks good')
     expect(page).to have_content(
-      I18n.t('solutions.show.solution_for_user', username: 'otheruser')
+      I18n.t('solutions.show.solution_for_user', username: 'other_user')
     )
     expect(page).to have_content(I18n.t('solutions.show.no_solution'))
   end
 
-  scenario 'sees list of all solutions' do
+  scenario 'and sees list of all solutions' do
     solution1, solution2 = create_pair(:solution)
     visit_solutions
     expect_to_see_solution(solution1)
     expect_to_see_solution(solution2)
   end
 
-  def visit_exercise(exercise)
-    sign_in_as_admin
+  def visit_solutions_for(options)
+    visit admin_root_path(as: options[:as])
     click_on I18n.t('admin.dashboards.show.exercises')
-    click_on exercise.title
+    click_on options[:exercise].title
+    click_on 'View Solutions'
   end
 
   def visit_solutions
@@ -39,5 +45,9 @@ feature 'admin views solutions' do
     expect(page).to have_content(solution.exercise.title)
     expect(page).to have_content(solution.user.username)
     expect(page).to have_content(solution.comments_count)
+  end
+
+  def reload_page
+    visit current_path
   end
 end

spec/models/review_spec.rb
@@ -226,23 +226,33 @@ describe Review do
   end
 
   describe '#user_is_awaiting_review?' do
-    context "when the user's solution has no comments" do
-      it "returns true" do
-        solution_with_no_comments =
-          stub_solution('no_comments_solution', has_comments?: false)
+    context 'when the user has submitted a solution' do
+      context "and the user's solution has no comments" do
+        it "returns true" do
+          solution_with_no_comments =
+            stub_solution('no_comments_solution', has_comments?: false)
 
-        review = build_review(submitted_solution: solution_with_no_comments)
+          review = build_review(submitted_solution: solution_with_no_comments)
 
-        expect(review.user_is_awaiting_review?).to be_true
+          expect(review.user_is_awaiting_review?).to be_true
+        end
+      end
+
+      context "and the user's solution has comments" do
+        it "returns false" do
+          solution_with_comments =
+            stub_solution('solution_with_comments', has_comments?: true)
+
+          review = build_review(submitted_solution: solution_with_comments)
+
+          expect(review.user_is_awaiting_review?).to be_false
+        end
       end
     end
 
-    context "when the user's solution has comments" do
-      it "returns false" do
-        solution_with_comments =
-          stub_solution('solution_with_comments', has_comments?: true)
-
-        review = build_review(submitted_solution: solution_with_comments)
+    context 'when the user has not submitted a solution' do
+      it 'returns false' do
+        review = build_review(submitted_solution: nil)
 
         expect(review.user_is_awaiting_review?).to be_false
       end