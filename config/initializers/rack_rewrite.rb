9d2d6d10d2eb32d05271f07a15a12f5dd2233cde
Joe Ferris <jferris@thoughtbot.com>
2014-08-01 12:45:30 -0400

    Force all requests to use APP_DOMAIN
    * rack-ssl-enforcer does not redirect https requests
    * We need to always use the same domain for OAuth2 redirects

---
+++ config/initializers/rack_rewrite.rb
@@ -0,0 +1,11 @@
+if ENV["APP_DOMAIN"].present?
+  require "rack/rewrite"
+
+  Rails.configuration.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+    r301(
+      %r{.*},
+      "http://#{ENV["APP_DOMAIN"]}$&",
+      if: -> (rack_env) { rack_env["SERVER_NAME"] != ENV["APP_DOMAIN"] }
+    )
+  end
+end
