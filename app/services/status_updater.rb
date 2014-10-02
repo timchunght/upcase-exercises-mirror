b549ddefba86bf05072fe6d4b89fda2e10145389
Chad Pytel <chad@thoughtbot.com>
2014-10-02 11:48:11 -0400

    reorder status updater methods

--- app/services/status_updater.rb
+++ app/services/status_updater.rb
@@ -1,16 +1,16 @@
 class StatusUpdater
   pattr_initialize [:user!, :exercise!, :upcase_client!]
 
-  def solution_submitted
-    update_with('Submitted')
-  end
-
   def clone_created
     update_with('Started')
   end
 
   def revision_submitted
     update_with('Pushed')
+  end
+
+  def solution_submitted
+    update_with('Submitted')
   end
 
   def comment_created
